6847521e333dbe7a001a96ac1d6774efeca83479
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-02-13 19:58:13 +0100

    a

--- MVCPELICULAS/Controllers/HomeController.cs
+++ MVCPELICULAS/Controllers/HomeController.cs
@@ -30,11 +30,32 @@ namespace MVCPELICULAS.Controllers
         
         public ActionResult _Login(String usuario,String pass)
         {
+            ViewBag.fallopw = "";
+            ViewBag.fallopwred = "";
             Session["TOVALIDAR"] = true;
             if(usuario=="admin"&& pass == "admin")
             {
                 Session["VALIDADO"] = true;
+            }            
+            else
+            {
+                Session["VALIDADO"] = null;
+                if ((usuario == null && pass == null))
+                {
+                    
+                }
+                if ( usuario == "" && pass == "")
+                {
+                    ViewBag.fallopw = "Introduzca usuario y pw";
+                }
+                if ((usuario != "admin" || pass != "admin")&&(usuario != "" && pass != "")
+                    && (usuario != null && pass != null))
+                {
+                    ViewBag.fallopwred = "Usuario o pw incorrectos";
+                }
+
             }
+
             return View();
         }
     }
